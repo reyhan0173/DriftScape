176e52f94f363818426585a172617240eb719f86
Oblivion0173 <113329952+Oblivion0173@users.noreply.github.com>
2023-10-12 05:31:02 -0600

    Added Car Movement
    Friction and Braking System

--- Source/RoomRacing/Core/Characters/MVehicleBase.cpp
+++ Source/RoomRacing/Core/Characters/MVehicleBase.cpp
@@ -6,6 +6,7 @@
 #include "MovieSceneSequenceID.h"
 #include "Components/ArrowComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 AMVehicleBase::AMVehicleBase()
@@ -51,6 +52,17 @@ AMVehicleBase::AMVehicleBase()
 void AMVehicleBase::BeginPlay()
 {
 	Super::BeginPlay();
+	//put them all in container
+	WheelArrowComponentHolder = {ArrowC_FR, ArrowC_FL, ArrowC_BL, ArrowC_BR};
+	//RestLength is axle length
+	MinLength = RestLength - SpringTravelLength;
+	MaxLength = RestLength + SpringTravelLength;
+	const FName TraceTag("MyTraceTag");
+	LineTraceCollisionQuery.TraceTag = TraceTag;
+	//Create visual debugging
+	LineTraceCollisionQuery.bDebugQuery = true;
+	//Ignore collisions with itself 
+	LineTraceCollisionQuery.AddIgnoredActor(this);
 	
 }
 
@@ -64,13 +76,102 @@ void AMVehicleBase::PostInitializeComponents()
 void AMVehicleBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	for(int WheelArrowIndex=0; WheelArrowIndex<4; WheelArrowIndex++)
+	{
+		UpdateVehicleForce(WheelArrowIndex, DeltaTime);
+	}
 
+}
+
+void AMVehicleBase::UpdateVehicleForce(int WheelArrowIndex, float DeltaTime)
+{
+	//Wheel arrow index selects the correct wheel to move and check collisions.
+	if(!WheelArrowComponentHolder.IsValidIndex(WheelArrowIndex))
+		return;
+
+	//Store the indexed arrow into the variable "arrow"
+	auto wheelArrowC = WheelArrowComponentHolder[WheelArrowIndex];
+	//Type FHitResult is a variable that stores the collisions and their types into the variable outHit
+	FHitResult outHit;
+	FVector startTraceLoc = wheelArrowC->GetComponentLocation();
+	FVector endTraceLoc = wheelArrowC->GetForwardVector()*(MaxLength + WheelRadius) + startTraceLoc;
+	//Create the trace using LineTraceSingleByChannel
+	GetWorld()->LineTraceSingleByChannel(outHit, startTraceLoc, endTraceLoc, ECC_Camera, LineTraceCollisionQuery, FCollisionResponseParams());
+	float PreviousSpringLength = SpringLength[WheelArrowIndex];
+	//these lines of code are used to obtain the local linear velocity of a physics-enabled component (BodyMeshC).
+	//This is useful for scenarios where you need to understand how a component is moving relative to its own orientation and position,
+	//which can be different from its motion in the global world coordinate system.
+	FVector WorldLinearVelocity = BodyMeshC->GetPhysicsLinearVelocity();
+	FVector localLinearVelocity = UKismetMathLibrary::InverseTransformDirection(BodyMeshC->GetComponentTransform(), WorldLinearVelocity);
+	if(outHit.IsValidBlockingHit())
+	{
+		float currentSpringLength = outHit.Distance - WheelRadius;
+		SpringLength[WheelArrowIndex] = UKismetMathLibrary::FClamp(currentSpringLength, MinLength, MaxLength);
+		float springVelocity = (PreviousSpringLength - SpringLength[WheelArrowIndex])/DeltaTime;
+		float springForce = (RestLength - SpringLength[WheelArrowIndex])*SpringForceConst;
+		float damperForce = springVelocity* DamperForceConst;
+		FVector upwardForce = GetActorUpVector()*(springForce + damperForce);
+		BodyMeshC->AddForceAtLocation(upwardForce, wheelArrowC->GetComponentLocation());
+	}
+
+	else
+	{
+		SpringLength[WheelArrowIndex] = MaxLength;
+	}
+
+	float CurrentSteeringAngle = UKismetMathLibrary::MapRangeClamped(RightAxisValue, -1, 1, MaxSteeringAngle*-1, MaxSteeringAngle);
+	if(WorldLinearVelocity.SizeSquared() > 1) //If car is moving, add rotational torque
+	{
+		BodyMeshC->AddTorqueInDegrees(FVector(0, 0, CurrentSteeringAngle), NAME_None, true);
+	}
+
+	FVector frictionVector = FVector::ZeroVector;
+	if(UKismetMathLibrary::Abs(localLinearVelocity.Y>2))
+	{
+		frictionVector = BodyMeshC->GetRightVector()*localLinearVelocity.Y*FrictionConst*-1;
+	}
+
+	FVector netForce = GetActorForwardVector()*ForwardAxisValue*ForwardForceConst + frictionVector; //forward force + friction force
+
+	if(BrakeApplied)
+	{
+		netForce = WorldLinearVelocity*-1*BrakeConst;
+	}
+	BodyMeshC->AddForce(netForce);
+}
+
+void AMVehicleBase::MoveForward(float Value)
+{
+	ForwardAxisValue = Value;
+}
+
+void AMVehicleBase::MoveRight(float Value)
+{
+	//Slowly add axis value, to have a smoother turn
+	UKismetMathLibrary::FInterpTo(RightAxisValue, Value, GetWorld()->GetDeltaSeconds(), 5);
+	RightAxisValue = Value;
+}
+
+void AMVehicleBase::BrakePressed()
+{
+	BrakeApplied = true;
+}
+
+void AMVehicleBase::BrakeReleased()
+{
+	BrakeApplied = false;
 }
 
 // Called to bind functionality to input
 void AMVehicleBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &AMVehicleBase::MoveForward);
+	PlayerInputComponent->BindAxis(FName("MoveRight"), this, &AMVehicleBase::MoveRight);
+	PlayerInputComponent->BindAction(FName("Brake"), IE_Pressed, this, &AMVehicleBase::BrakePressed);
+	PlayerInputComponent->BindAction(FName("Brake"), IE_Released, this, &AMVehicleBase::BrakeReleased);
 
 }
+
+
 
--- Source/RoomRacing/Core/Characters/MVehicleBase.h
+++ Source/RoomRacing/Core/Characters/MVehicleBase.h
@@ -34,7 +34,28 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	UCameraComponent* CameraComponent;
-	
+
+	//Exposed Variable
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	float RestLength = 50;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	float SpringTravelLength = 20;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	float WheelRadius = 30;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	float SpringForceConst = 50000;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	float DamperForceConst = 5000;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	float ForwardForceConst = 100000;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	float MaxSteeringAngle = 30;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	float FrictionConst = 500;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	float BrakeConst = 1000;
+
+
 	// Sets default values for this pawn's properties
 	AMVehicleBase();
 
@@ -43,11 +64,37 @@ protected:
 	virtual void BeginPlay() override;
 	virtual void PostInitializeComponents() override;
 
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	bool BrakeApplied;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+
+	
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	TArray<UArrowComponent*> WheelArrowComponentHolder;
+	float MinLength;
+	float MaxLength;
+	FCollisionQueryParams LineTraceCollisionQuery;
+	float SpringLength[4] = {0,0,0,0};
+	float ForwardAxisValue;
+	float RightAxisValue;
+
+	
+
+private:
+	void UpdateVehicleForce(int WheelArrowIndex, float DeltaTime);
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void BrakePressed();
+	void BrakeReleased();
+	
 };
+
+
